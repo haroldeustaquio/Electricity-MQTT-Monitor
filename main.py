c0a1b858e7deda9fd0a0f7bfb486b280afac3eb4
harold1735 <harold.eustaquio.a@uni.pe>
2024-11-10 10:37:45 -0600

    update main

---
+++ main.py
@@ -0,0 +1,17 @@
+import paho.mqtt.client as mqtt
+from functions import mqtt_extraction
+
+broker, port, topic = mqtt_extraction.return_broker_data()
+
+
+# Create the client MQTT
+client = mqtt.Client()
+
+client.on_connect = mqtt_extraction.on_connect
+client.on_message = mqtt_extraction.on_message
+
+client.connect(broker, port)
+
+
+# Mantain the client in loop to recibe messages
+client.loop_forever()
