262e8b218a98179c686a57926ec848478b981d2b
haroldeustaquio <harold.eustaquio.a@uni.pe>
2024-11-16 00:28:42 -0600

    update outliers detectors

---
+++ monitor/functions/voltage_detector.py
@@ -0,0 +1,80 @@
+from functions import file_handling
+import numpy as np
+from scipy.stats import iqr, shapiro
+from statsmodels.tsa.arima.model import ARIMA
+from sklearn.ensemble import IsolationForest
+
+def iqr_detection(list_voltage,instant_energy,iqr_factor = 1.5):
+    Q1 = np.percentile(list_voltage, 25)
+    Q3 = np.percentile(list_voltage, 75)
+    IQR = iqr(list_voltage)
+    
+    lim_inf = Q1 - iqr_factor * IQR
+    lim_sup = Q3 + iqr_factor * IQR
+    
+    voltage_alert = (instant_energy >= lim_inf) & (instant_energy <= lim_sup)
+    
+    return voltage_alert
+
+def z_score_detection(list_voltage, instant_energy, threshold = 3):
+    _, p_value = shapiro(list_voltage)
+    
+    if p_value > 0.05:
+        mean = np.mean(list_voltage)
+        std = np.std(list_voltage)
+        
+        z_score = abs((instant_energy - mean)/std)
+        voltage_alert = z_score > threshold
+    else:
+        voltage_alert = 0
+    
+    return voltage_alert
+
+def iso_for_detection(list_voltage, instant_energy):
+    iso_for = IsolationForest(contamination=0.05)
+    iso_for.fit(np.array(list_voltage).reshape(-1,1))
+    
+    value = iso_for.predict([[instant_energy]])
+    voltage_alert = True if value == -1 else False
+    
+    return voltage_alert
+
+def arima_detection(list_voltage,instant_energy):
+    model = ARIMA(list_voltage, order=(0,1,0))
+    model_fit = model.fit()
+
+    y_pred = model_fit.forecast(steps=1)[0]
+    error = abs(instant_energy - y_pred)
+    threshold = 0.05 * y_pred
+    
+    voltage_alert = True if error > threshold else False
+    
+    return voltage_alert
+
+def volt_outliers(instant_energy):
+    data = file_handling.read_json('voltage','electrical-data')
+    
+    list_date = []
+    list_voltage = []
+    
+    # Select the last hour of data
+    for x in data[-31:-1]:
+        list_voltage.append(x['instant_energy'])
+        list_date.append(x['date'])
+    
+    iqr_alert = iqr_detection(list_voltage,instant_energy)
+    
+    z_alert = z_score_detection(list_voltage, instant_energy, threshold = 3)
+    
+    iso_alert = iso_for_detection(list_voltage,instant_energy)
+    
+    arima_alert = arima_detection(list_voltage,instant_energy)
+    
+    sum_alert = iqr_alert + z_alert + iso_alert + arima_alert
+    
+    print('iqr: ',iqr_alert)
+    print('z: ',z_alert)
+    print('isolation: ',iso_alert)
+    print('arima: ',arima_alert)
+    
+    return 1 if sum_alert >= 2 else 0
