c49d507540b2c0eea89ad1b9319b6d3f541df091
haroldeustaquio <harold.eustaquio.a@uni.pe>
2024-11-26 05:52:40 -0600

    drop notifc scripts

--- notifications/keepAlive.js
+++
@@ -1,6 +0,0 @@
-// keepAlive.js
-module.exports = (client) => {
-    setInterval(() => {
-        console.log('Keeping session alive...');
-    }, 300000); // Cada 5 minutos
-};
--- notifications/sendMessage.js
+++
@@ -1,9 +0,0 @@
-// sendMessage.js
-module.exports = async (client, number, message) => {
-    try {
-        await client.sendMessage(number, message);
-        console.log('Message sent successfully');
-    } catch (error) {
-        console.error('Error sending message:', error);
-    }
-};
